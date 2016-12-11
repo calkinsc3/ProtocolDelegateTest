846c77c53a5e94acac51707dd62e48b666ec869e
Bill Calkins <calkinsc3@gmail.com>
2016-12-11 13:41:46 -0600

    Added test table viewcontroller.

---
+++ ProtocolDelegateTester/ProtocolDelegateTester/TableVewTestController.swift
@@ -0,0 +1,74 @@
+//
+//  TableVewTestController.swift
+//  ProtocolDelegateTester
+//
+//  Created by William Calkins on 12/11/16.
+//  Copyright © 2016 Calkins Computer Consulting. All rights reserved.
+//
+
+import UIKit
+
+class TableVewTestController: UITableViewController {
+    
+    let dataArray = ["Bill", "Danny", "Khalid", "Annie", "Kevin", "Paul"]
+    var containerVC: UIViewController?
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let container = self.containerVC {
+            container.view.isHidden = true
+            self.containerView.isHidden = true
+            self.containerView.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+            
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.dataArray.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TestCell", for: indexPath)
+        cell.textLabel?.text = self.dataArray[indexPath.row]
+        return cell
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "containerSegue" {
+            if let containerVC = segue.destination as? UIViewController {
+                self.containerVC = containerVC
+            }
+        }
+        
+    }
+
+
+}
