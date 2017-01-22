b6596d4688e4cc4d32f71e8ac73387f911648026
Bill Calkins <calkinsc3@gmail.com>
2017-01-22 06:58:49 -0600

    Added toast animations

--- ProtocolDelegateTester/ProtocolDelegateTester/FirstViewController.swift
+++ ProtocolDelegateTester/ProtocolDelegateTester/FirstViewController.swift
@@ -79,23 +79,66 @@ class FirstViewController: UIViewController {
     
     @IBAction func showToast(_ sender: Any) {
         
-        if let toastView = ToastView(toastYPosition: self.view.frame.height - 90, toastWidth: self.view.frame.width) {
+        if let toastView = ToastView(toastYPosition: self.view.frame.height - 100, toastWidth: self.view.frame.width) {
             toastView.displayText = "Your password has been changed"
+            toastView.translatesAutoresizingMaskIntoConstraints = false
             
-            UIView.transition(with: toastView, duration: 20.0, options: [.curveEaseIn], animations: {
-                self.view.addSubview(toastView)
-                
-            }, completion: { (result) in
-                print("in the completion view")
+            //Add the toast view 
+            self.view.addSubview(toastView)
+            
+            delay(seconds: 3.0, completion: {
+                UIView.transition(with: toastView, duration: 0.6, options: [.curveEaseIn], animations: {
+                    self.view.addSubview(toastView)
+                    
+                }, completion: { (result) in
+                    toastView.removeFromSuperview()
+                })
             })
             
-            
         }
-        
     }
     
+    func showItem(_ index: Int) {
+        //show the selected image
+        let imageView  = UIImageView(image: UIImage(named: "summericons_100px_0\(index).png"))
+        imageView.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5)
+        imageView.layer.cornerRadius = 5.0
+        imageView.layer.masksToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(imageView)
+        
+        let conX = imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        let conBottom = imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: imageView.frame.height)
+        let conWidth = imageView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.33, constant: -50.0)
+        let conHeight = imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+        
+        NSLayoutConstraint.activate([conX, conBottom, conWidth, conHeight])
+        self.view.layoutIfNeeded()
+        
+        conBottom.constant = -imageView.frame.size.height/2
+        conWidth.constant = 0.0
+        
+        UIView.animate(withDuration: 0.33, delay: 0.0,
+                       usingSpringWithDamping: 0.6, initialSpringVelocity: 10.0,
+                       options: [.curveEaseOut], animations: {
+                        self.view.layoutIfNeeded()
+        }, completion: nil)
+        
+        UIView.animate(withDuration: 0.67, delay: 2.0,
+                       options: [], animations: {
+                        conBottom.constant = imageView.frame.size.height
+                        conWidth.constant = -50.0
+                        self.view.layoutIfNeeded()
+        }, completion: {_ in
+            imageView.removeFromSuperview()
+        })
+    }
     
 }
+
+
+
 
 extension FirstViewController: afiDataAccessDelegate {
     
@@ -110,6 +153,7 @@ extension FirstViewController: afiDataAccessDelegate {
 }
 
 func delay(seconds: Double, completion:@escaping ()->()) {
+    
     let popTime = DispatchTime.now() + Double(Int64( Double(NSEC_PER_SEC) * seconds )) / Double(NSEC_PER_SEC)
     
     DispatchQueue.main.asyncAfter(deadline: popTime) {
